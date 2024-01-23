5846e8fc5a681e21f910f1c21aba6791dbe3c677
José Luciano Julietti Rodrigues de Castro <joselucianorc@gmail.com>
2024-01-23 15:04:33 -0300

    Changing project structure

---
+++ src/br/com/joselucianorc/dsa/challenge/Solution001.java
@@ -0,0 +1,24 @@
+package br.com.joselucianorc.dsa.challenge;
+
+import java.util.*;
+
+public class Solution001 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String string = scan.nextLine();
+
+        string = string.replace("'"," ")
+                .replace(","," ")
+                .replace("?"," ")
+                .replace("!"," ");
+
+        StringTokenizer tokenizer = new StringTokenizer(string, " ");
+        System.out.println(tokenizer.countTokens());
+
+        while(tokenizer.hasMoreElements()) {
+            System.out.println(tokenizer.nextElement());
+        }
+        scan.close();
+    }
+}
